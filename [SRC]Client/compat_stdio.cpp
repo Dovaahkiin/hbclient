ad139c8840336f9539c36584fabd99308dee54c0
Dovaahkiin <73516338+Dovaahkiin@users.noreply.github.com>
2025-06-16 13:20:36 -0300

    Fix __iob compatibility for new MSVC

--- [SRC]Client/compat_stdio.cpp
+++ [SRC]Client/compat_stdio.cpp
@@ -2,8 +2,12 @@
 #include <stdio.h>
 
 // Provide the deprecated __iob symbol used by older libraries
+#if _MSC_VER >= 1400
+extern "C" FILE* __imp___iob_func = __acrt_iob_func(0);
+#else
 extern "C" FILE _iob[] = { *stdin, *stdout, *stderr };
 extern "C" FILE* __imp___iob = _iob;
+#endif
 
 // Map legacy import variables for common stdio routines
 extern "C" int (__cdecl* __imp__printf)(const char*, ...)   = &printf;
